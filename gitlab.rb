9ce360b1cfd7c923d8f0607b0b15a2724c38c48b
Fabien Barbaud <fabien.barbaud@timeonegroup.com>
2021-03-11 00:39:01 +0100

    fix: network spec

--- gitlab.rb
+++ gitlab.rb
@@ -1,2 +1,5 @@
-external_url 'http://localhost/'
+external_url 'http://localhost'
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password')
+# gitlab_rails['registry_enabled'] = true
+# gitlab_rails['registry_host'] = "localhost"
+# gitlab_rails['registry_port'] = "5005"
